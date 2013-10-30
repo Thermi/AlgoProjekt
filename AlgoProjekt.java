5559dfec5a8f3540cffe8a407012755d5190c785
Thermi <noel@familie-kuntze.de>
2013-10-30 19:33:55 +0100

    - Initial commit.

---
+++ AlgoProjekt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoprojekt;
+
+/**
+ *
+ * @author thermi
+ */
+import java.util.Hashtable;
+import java.util.Random;
+
+public class AlgoProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
