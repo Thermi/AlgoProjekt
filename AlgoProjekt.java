5e53e9a909b595d5e6c53170c547e439de08e764
Thermi <noel@familie-kuntze.de>
2013-11-06 19:20:41 +0100

    -Implemented OnlinePhase.testRainbowTable -Modified Readme.md to describe the program -Modified the main method in AlgoProjekt.java

--- AlgoProjekt.java
+++ AlgoProjekt.java
@@ -8,15 +8,20 @@ package algoprojekt;
  *
  * @author thermi
  */
-import java.util.Hashtable;
-import java.util.Random;
-
+import java.util.*;
+import java.lang.*;
+import java.security.MessageDigest;
+import java.io.FileInputStream;
 public class AlgoProjekt {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* we create a hashtable which uses the hash of the password as the key
+         * to get the password from the hashtable */
+        Hashtable<byte[], String> table = new Hashtable<byte[], String>();
+        // more is to be written.
+
     }
 }
